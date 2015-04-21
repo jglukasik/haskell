87fb4bc0de321b8f49e746b6aea6c0eec328feff
Joseph Lukasik <jglukasik@wisc.edu>
2015-04-20 19:59:05 -0500

    Use a fold to check if points are within circle

--- hacks/prob/pi.hs
+++ hacks/prob/pi.hs
@@ -1,6 +1,11 @@
 import System.Random
 import System.Environment
 
+--TODO:
+--  - With n > ~200000, we get a stack space overflow. It might be the case that
+--  somewhere I'm not using tail recursion, forcing the program to keep a huge
+--  list of numbers in the stack instead of getting rid of the head of the list
+--  after we use it.
 
 unif :: StdGen -> [Double]
 unif g = randoms g
@@ -18,9 +23,14 @@ inQuarterCircle coords = [if radius (x,y) < 1.0 then 1 else 0
 approxPi :: Int -> StdGen -> Double
 approxPi n g = fromIntegral numInCircle / fromIntegral n * 4.0
   where
-    numInCircle = sum (take n . inQuarterCircle . coords . unif $ g) 
+    numInCircle = n - (sumOutCircle . take n .  coords . unif $ g) 
 
 main = do
   g <- getStdGen
   (n:_) <- getArgs
   print $ approxPi (read n :: Int) g
+
+
+sumOutCircle :: [(Double, Double)] -> Int
+sumOutCircle nums = foldl (\acc (x,y) -> acc + floor ((x**2 + y**2)**0.5) ) 0 nums
+
