661bc1df4eec30d9241c2fc3c648dc071a8bb203
Joseph Lukasik <jglukasik@wisc.edu>
2015-04-27 01:14:49 -0500

    Corrected Graham Scan algorithm, added functions for dealing with points

--- ex/rwh/ch3.hs
+++ ex/rwh/ch3.hs
@@ -1,4 +1,5 @@
 -- Exercises from Chapter 3 of Real World Haskell
+import System.Random
 import Data.List
 
 makePalindrome :: [a] -> [a]
@@ -24,8 +25,18 @@ data Point = Point
              , y :: Double
              } deriving (Eq, Show)
 
-pointsToTuple :: [Point] -> [(Double, Double)]
-pointsToTuple ps = [ ((x p), (y p)) | p <- ps]
+tuplesToPoints :: [(Double,Double)] -> [Point]
+tuplesToPoints ts = [Point x y | (x,y) <- ts]
+
+pointsToTuples :: [Point] -> [(Double, Double)]
+pointsToTuples ps = [ ((x p), (y p)) | p <- ps]
+
+unif :: StdGen -> [Double]
+unif g = randoms g
+
+coords :: [Double] -> [(Double, Double)]
+coords (x:y:r) = (x,y):coords r
+coords _ = []
 
 getDirection :: Point -> Point -> Point -> Direction
 getDirection a b c | crossProductZ > 0  = LeftT
@@ -46,16 +57,16 @@ getMinPoint ps = head (sortBy byX ps)
           | (x p1) > (x p2) = GT
           | (x p1) < (x p2) = LT
           | otherwise       = (y p1) `compare` (y p2)
-      
 
-getConvexPoints :: [Point] -> [Point]
-getConvexPoints (a:b:c:xs)
-        | dir == RightT = getConvexPoints (a:c:xs)
-        | otherwise     = a : (getConvexPoints (b:c:xs) )
+getConvexPoints :: [Point] -> [Point] -> [Point]
+getConvexPoints (b:a:stack) (x:xs)
+        | dir == RightT = getConvexPoints (a:stack) (x:xs)
+        | otherwise     = getConvexPoints (x:b:a:stack) (xs)
       where
-        dir = getDirection a b c
-getConvexPoints (a:b:[]) = [a,b]
-getConvexPoints _ = []
+        dir = getDirection a b x
+getConvexPoints (a:[]) (x:xs) = getConvexPoints [x,a] xs
+getConvexPoints [] (x:xs) = getConvexPoints [x] xs
+getConvexPoints stack [] = stack
 
 sortByAngleWith :: Point -> [Point] -> [Point]
 sortByAngleWith p ps = sortBy byAngle ps
@@ -63,16 +74,6 @@ sortByAngleWith p ps = sortBy byAngle ps
         angleBetween b pn = atan $ ((y pn) - (y b)) / ((x pn) - (x b))
 
 grahamScan :: [Point] -> [Point]
-grahamScan ps = getConvexPoints $ minP : sortByAngleWith minP rest
+grahamScan ps = getConvexPoints [minP] (sortByAngleWith minP rest)
   where minP = getMinPoint ps
         rest = delete minP ps
-
-grahamScan' :: [Point] -> [Point]
-grahamScan' ps = minP : sortByAngleWith minP rest
-  where minP = getMinPoint ps
-        rest = delete minP ps
-
--- Example for testing
-pTuples = [(0,4), (1,1), (2,2), (4,4), (0,0), (1,2), (3,1), (3,3)] 
-          :: [(Double,Double)]
-ps = [Point x y | (x,y) <- pTuples]
