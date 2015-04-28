2648015f54acafc2e881197c241c6357b5c80097
Joseph Lukasik <jglukasik@wisc.edu>
2015-04-28 11:46:30 -0500

    Perturbations to growth speed, random colors

--- hacks/pics/bubbles.hs
+++ hacks/pics/bubbles.hs
@@ -17,32 +17,41 @@ bubbles :: StdGen -> ProcScript
 bubbles g = animateFigure Nothing Nothing 50 (Color 0 0 0 255) (bubblesf g)
 
 radius :: Proc_Float
-radius = 50
+radius = 40
 
 speed :: Proc_Float
-speed = 20
+speed = 15
 
 scale :: Proc_Float
-scale = 100
+scale = 150
 
 numCircles :: Int
-numCircles = 30
-
---pToProc scale p = (convert $ fst p, convert $ snd p)
+numCircles = 90
 
 bubblesf :: StdGen -> Proc_Int -> Figure
-bubblesf g n =
- let t = (sin $ (intToFloat $ n) / speed ) * radius
-     ps = map convert (take numCircles . normals . unif $ g)
-     --rs = map ((* (radius / 10)) . (+ t) . fst) 
-     --         (take numCircles . normals . unif $ g)
-     rs = replicate numCircles t
-     convert p = (move $ fst p, move $ snd p)
-     move x = (+) (-scale/2) $ (*) scale x
- in  FillColor (Color 100 255 240 50)
-   $ LineColor (Color 255 255 255 255)
-   $ mconcat 
-   $ (map . uncurry $ Circle) (zip ps rs)
+bubblesf g n = FillColor (Color 100 255 240 50) 
+             $ LineColor (Color 255 255 255 255) 
+             $ mconcat 
+             $ zipWith FillColor 
+                       (take numCircles . randColor $ g)
+                       $ (map . uncurry $ Circle) (zip ps rs)
+ where rs = map ( (* radius)
+                . sin
+                . (* (1/speed)) 
+                . (+ intToFloat n) 
+                . (* scale) 
+                . fst 
+                ) (take numCircles . normals . unif $ g)
+       ps = map convert (take numCircles . normals . unif $ g)
+       convert p = (move $ fst p, move $ snd p)
+       move x = (+) (-scale/2) $ (*) scale x
+
+-- Infinite stream of random colors
+randColor :: StdGen -> [Color]
+randColor g = makeC (randoms g :: [Float])
+  where makeC (r:b:g:xs) = Color (s r) (s b) (s g) 100 : makeC xs
+        makeC _ = []
+        s = fromInt . floor . (* 255)
 
 -- Infinte stream of uniformly generated points
 unif :: StdGen -> [Proc_Point]
