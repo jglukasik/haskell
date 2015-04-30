df5c3802ef98eb01d029ec43e0d78c028ddf8b57
Joseph Lukasik <jglukasik@wisc.edu>
2015-04-29 19:09:53 -0500

    Made moving triangles in square

--- hacks/pics/square.hs
+++ hacks/pics/square.hs
@@ -7,27 +7,56 @@ import System.Random
 -- Main function. Saves html and data file for processing animation
 main :: IO ()
 main = do
+  g <- newStdGen
   writeHtml "processing.min.js" 
             "square.pde" 
             "square demo" 
             "square.html" 
-            $ square
+            $ square g
 
 -- Scaling factor
 s :: Proc_Float
-s = 300
+s = 200
 
-square :: ProcScript
-square = animateFigure Nothing
-                       Nothing
-                       50
-                       (Color 0 0 0 255)
-                       squaref
+square :: StdGen -> ProcScript
+square  g = animateFigure Nothing
+                          Nothing
+                          50
+                          (Color 0 0 0 255)
+                          (squaref g)
 
-squaref :: Proc_Int -> Figure
-squaref t = Polygon triangle
-   where triangle = [(s,s),(-s,s),(s,-s)]
+squaref :: StdGen -> Proc_Int -> Figure
+squaref g t = FillColor (Color 130 20 200 150)
+                        (Polygon t1)
+           <> FillColor (Color 50 250 170 150) 
+                        (Polygon t2)
+           <> FillColor (Color 170 50 110 150) 
+                        (Polygon t3)
+           <> FillColor (Color 10 150 210 150) 
+                        (Polygon t4)
+   where t1 = [(s,s),p1,p2]
+         t2 = [(-s,s),p3,p4]
+         t3 = [(-s,-s),p5,p6]
+         t4 = [(s,-s),p7,p8]
+         p1 = (s, s*sin(f*t'))
+         p2 = (s*cos(f*t'), s)
+         p3 = (-s, s*cos(f*t'))
+         p4 = (s*sin(f*t'), s)
+         p5 = (-s, s*sin(f*t'))
+         p6 = (s*cos(f*t'), -s)
+         p7 = (s, s*cos(f*t'))
+         p8 = (s*sin(f*t'), -s)
+         t' = intToFloat t
+         f = (1/13)
+   
 
 -- Map over tuple elements
 mapT :: (a -> b) -> (a, a) -> (b, b)
 mapT f (x,y) = (f x, f y)
+--
+-- Infinte stream of uniformly generated points
+unif :: StdGen -> [Proc_Point]
+unif g = makeP (randoms g)
+  where makeP (x:y:r) = mapT fromFloat (x,y):makeP r
+        makeP _ = []
+
